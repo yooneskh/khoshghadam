34f86512972f230da6c742efd1597a5cd401be63
Yoones Khoshghadam <yooneskh@gmail.com>
2025-07-14 00:03:22 +0300

    chore: restructure app config, improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
-ssh $zxc mkdir -p /home/yooneskh/khoshghadam && \
-  cd ./frontend/ && bun run build && rsync -rvzh --delete ./.output/* $zxc:/home/yooneskh/khoshghadam/frontend/ && cd ../ && \
-  scp ./ecosystem.prod.config.js $zxc:/home/yooneskh/khoshghadam/ && \
-  ssh $zxc "cd /home/yooneskh/khoshghadam && pm2 restart /home/yooneskh/khoshghadam/ecosystem.prod.config.js --update-env"
+ssh $zxc mkdir -p /home/yooneskh/khoshghadam
+
+(
+  (
+    cd ./frontend/ && bun run build && rsync -rvzh --delete ./.output/* $zxc:/home/yooneskh/khoshghadam/frontend/
+  ) & 
+  (
+    scp ./ecosystem.prod.config.js $zxc:/home/yooneskh/khoshghadam/
+  ) &
+  wait
+)
+
+ssh $zxc "cd /home/yooneskh/khoshghadam && pm2 restart /home/yooneskh/khoshghadam/ecosystem.prod.config.js --update-env"
---
+++ frontend/addons/unified-app/app/app.config.ts
@@ -0,0 +1,19 @@
+
+
+export default defineAppConfig({
+  ui: {
+
+    card: {
+      slots: {
+        body: 'p-3 sm:p-3',
+      },
+    },
+
+    button: {
+      slots: {
+        base: 'cursor-pointer',
+      },
+    },
+
+  },
+});

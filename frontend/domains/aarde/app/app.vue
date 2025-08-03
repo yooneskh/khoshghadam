fdb8c14c294b99aeff6c99c679a7c884d9b7d418
Yoones Khoshghadam <yooneskh@gmail.com>
2025-08-03 22:20:21 +0300

    feat (seo): add jsonld

--- frontend/domains/aarde/app/app.vue
+++ frontend/domains/aarde/app/app.vue
@@ -13,6 +13,30 @@ useSeoMeta({
   'description': 'Portfolio reimagined as an operating system! My creative showcase of web development skills using Vue, Nuxt and Bun',
 });
 
+useJsonld({
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  name: config.brand.title,
+  url: 'https://khoshghadam.com',
+  description: 'Portfolio reimagined as an operating system! My creative showcase of web development skills using Vue, Nuxt and Bun',
+  publisher: {
+    "@type": "Person",
+    name: 'Yoones Khoshghadam',
+  },
+  author: {
+    "@type": "Person",
+    name: 'Yoones Khoshghadam',
+  },
+  creator: {
+    "@type": "Person",
+    name: 'Yoones Khoshghadam',
+  },
+  copyrightHolder: {
+    "@type": "Person",
+    name: 'Yoones Khoshghadam',
+  },
+});
+
 </script>
 
 

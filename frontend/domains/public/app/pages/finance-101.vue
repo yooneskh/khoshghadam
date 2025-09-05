09df79d1fa9a6be3dacee566804cbebc98eba751
Yoones Khoshghadam <yooneskh@gmail.com>
2025-09-05 15:45:30 +0300

    feat (finance): add base page

---
+++ frontend/domains/public/app/pages/finance-101.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+/* page */
+
+definePageMeta({
+  name: 'public.finance-101',
+});
+
+useSeoMeta({
+  title: 'Finance 101',
+  description: 'Essential concepts of finance alongside their live calculators for you to see exactly what is going on.',
+});
+
+</script>
+
+
+<template>
+  <window-base pito="event-viewer" title="Finance 101">
+    
+    <h1 class="text-4xl font-bold px-3 my-3">
+      Finance 101
+    </h1>
+
+  </window-base>
+</template>

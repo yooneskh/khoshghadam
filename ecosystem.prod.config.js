5d47e5847875ca7e0dbd4d575fc0f4eeee97485a
Yoones Khoshghadam <yooneskh@gmail.com>
2025-10-22 10:23:44 +0300

    chore: serve configuration

--- ecosystem.prod.config.js
+++ ecosystem.prod.config.js
@@ -2,8 +2,8 @@ module.exports = [
   {
     name: 'khoshghadam-frontend',
     cwd: './frontend/',
-    script: 'bun',
-    args: 'run ./server/index.mjs',
+    script: 'mise',
+    args: 'exec bun@1.3.0 -- bun run ./server/index.mjs',
     env: {
       NODE_ENV: 'production',
       PORT: 33500,

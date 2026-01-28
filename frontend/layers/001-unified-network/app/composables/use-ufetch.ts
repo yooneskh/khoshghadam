d0e33903102945a69edfad8537393cebb65392b5
Yoones Khoshghadam <yooneskh@gmail.com>
2026-01-28 23:14:31 +0300

    chore: refactor to new structure

--- frontend/layers/001-unified-network/app/composables/use-ufetch.ts
+++
@@ -1,8 +0,0 @@
-
-
-export function useUFetch(request: Parameters<typeof useFetch>[0], options?: Parameters<typeof useFetch>[1] & { silent?: boolean }) {
-  return useFetch(request, {
-    ...(options || {}),
-    $fetch: useNuxtApp().$api,
-  });
-}

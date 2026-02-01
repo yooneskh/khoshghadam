a1d0f97d05661d4fc7e7ef411191de49660b5192
Yoones Khoshghadam <yooneskh@gmail.com>
2026-02-01 03:53:16 +0300

    chore: test create user

--- layers/103-users/server/api/users.post.ts
+++ layers/103-users/server/api/users.post.ts
@@ -1,15 +1,20 @@
 
 
 export default defineEventHandler(async event => {
+  try {
 
-  const db = await loadDbClient(event);
-  const users = db.collection('users');
+    const db = await loadDbClient(event);
+    const users = db.collection('users');
 
-  return users.insertOne({
-    _id: '2' as any,
-    username: 'yooneskh',
-    password: 'password',
-    createdAt: Date.now(),
-  });
+    return users.insertOne({
+      _id: '2' as any,
+      username: 'yooneskh',
+      password: 'password',
+      createdAt: Date.now(),
+    });
 
+  }
+  catch (error) {
+    return error;
+  }
 });

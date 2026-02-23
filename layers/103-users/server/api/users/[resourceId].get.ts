d9402a6bc42213f70c805e4f1c00bac7d6f08f2f
Yoones Khoshghadam <yooneskh@gmail.com>
2026-02-23 16:49:03 +0300

    chore (unified resources): improve unified routing structure

--- layers/103-users/server/api/users/[resourceId].get.ts
+++
@@ -1,8 +0,0 @@
-
-
-export default defineEventHandler(async event => {
-  return handleUnifiedRetrieveRoute({
-    event,
-    controller: event.context.users,
-  });
-});
---
+++ layers/103-users/server/api/users/index.ts
@@ -0,0 +1,5 @@
+
+
+export default createResourceRouter({
+  resource: 'users',
+});

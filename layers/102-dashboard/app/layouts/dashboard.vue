6695c43ee8d2587e8d873ffcd65b8ced134bd660
Yoones Khoshghadam <yooneskh@gmail.com>
2026-04-23 16:41:53 +0300

    fix (dashboard): layout improvement

--- layers/102-dashboard/app/layouts/dashboard.vue
+++ layers/102-dashboard/app/layouts/dashboard.vue
@@ -7,7 +7,7 @@
   <div class="bg-muted min-h-dvh">
 
     <header class="bg-primary-950 text-inverted pb-14.5">
-      <div class="max-w-7xl mx-auto py-4">
+      <div class="w-440 max-w-[calc(100vw-16px)] mx-auto py-4">
         <div class="flex items-center gap-3 px-3">
 
           <img
@@ -34,7 +34,7 @@
               },
             ]">
             <u-button
-              color="primary"
+              variant="subtle"
               icon="lucide:user"
             />
           </u-dropdown-menu>
@@ -43,7 +43,7 @@
       </div>
     </header>
 
-    <main class="max-w-7xl mx-auto bg-default border border-default shadow-lg shadow-neutral-200 rounded-xl -mt-14.5">
+    <main class="w-440 max-w-[calc(100vw-16px)] mx-auto bg-default border border-default shadow-lg shadow-neutral-200 rounded-xl -mt-14.5">
 
       <u-navigation-menu
         :items="[

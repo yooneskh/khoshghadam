f21f9628407f5230c29c8bbe3c17a4c44c8e97ee
Yoones Khoshghadam <yooneskh@gmail.com>
2026-08-12 08:23:53 +0300

    fix (flash cards): add journeys to sitemaps

--- layers/100-aarde/server/api/sitemap-urls.ts
+++ layers/100-aarde/server/api/sitemap-urls.ts
@@ -4,11 +4,13 @@ export default defineSitemapEventHandler(async () => {
 
   const data = await Promise.all([
     $fetch('/api/flash-cards') as Promise<any[]>,
+    $fetch('/api/flash-card-journeys') as Promise<any[]>,
   ]);
 
 
   return [
     ...data[0]!.map(it => `/flash-cards/${it.slug}`),
+    ...data[1]!.map(it => `/flash-card-journeys/${it.slug}`),
   ].flat(Infinity);
 
 });

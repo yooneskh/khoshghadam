83e4d2bd6613a96b6471833bd1a12b24a3bdc67e
Yoones Khoshghadam <yooneskh@gmail.com>
2026-08-30 20:06:35 +0300

    fix (omani): better vote featuring

--- layers/203-omani/server/api/omani/featured.get.ts
+++ layers/203-omani/server/api/omani/featured.get.ts
@@ -29,7 +29,7 @@ export default defineEventHandler(async event => {
   }
 
 
-  const [candidate] = await app.omaniIdeas.dbo.list({
+  let [candidate] = await app.omaniIdeas.dbo.list({
     filter: {
       status: 'published',
       featuredOn: {
@@ -45,6 +45,59 @@ export default defineEventHandler(async event => {
       author: ['name', 'username'],
     },
   });
+
+
+  if (!candidate) {
+
+    const publishedCount = await app.omaniIdeas.dbo.count({
+      filter: {
+        status: 'published',
+      },
+    });
+
+    if (!publishedCount) {
+      return null;
+    }
+
+
+    const previouslyFeatured = await app.omaniIdeas.dbo.list({
+      filter: {
+        featuredOn: {
+          $exists: true,
+        },
+      },
+    });
+
+    await Promise.all(
+      previouslyFeatured.map(idea =>
+        app.omaniIdeas.dbo.updateQuery({
+          resourceId: idea._id,
+          query: {
+            $unset: {
+              featuredOn: 1,
+            },
+          },
+        }),
+      ),
+    );
+
+
+    [candidate] = await app.omaniIdeas.dbo.list({
+      filter: {
+        status: 'published',
+      },
+      sort: {
+        voteCount: -1,
+        createdAt: -1,
+      },
+      limit: 1,
+      populate: {
+        author: ['name', 'username'],
+      },
+    });
+
+  }
+
 
   if (!candidate) {
     return null;
